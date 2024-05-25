19c00cb27a646b41a00a2aaa705e0e39d4929dfb
jeferson.rodrigues <jeferson.rodrigues@cnj.jus.br>
2024-05-25 14:38:49 -0300

    Atualizacao

--- tests/test_2_search_product.py
+++ tests/test_2_search_product.py
@@ -7,8 +7,6 @@ class Test_Search_Products:
         home_page.open_home_page()
         search_page = SearchPage(home_page.driver)
 
-
-
         home_page.realizar_pesquisa_de_produto('Science')
         assert search_page.is_url_seach_page(), 'URL incorreta'
         assert search_page.verificar_resultado_da_pesquisa(), 'O produto nao foi apresentado'
