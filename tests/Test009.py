8fe01a24b4afb1454f4b97f38a4956ed50600b5d
lidianevm <lidianevalentim_@hotmail.com>
2024-05-25 16:28:49 -0300

    atualizacao nome do metodo

--- tests/Test009.py
+++ tests/Test009.py
@@ -4,9 +4,10 @@ from pages.WishListPage import WishListPage
 
 class Test009:
 
-    def test_add_product_to_wishlist(self, setup):
+    def submit_forms_on_contact_us(self, setup):
         home_page = setup
         home_page.open_home_page()
         contactus_page = ContactUsPage(home_page.driver)
         home_page.click_on_contactUs()
         contactus_page.contact_us_page("Lidiane", "lidiane@teste.com", "Lorem Ipsum is simply dummy text of the printing and typesetting industry.")
+
