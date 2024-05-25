bd3041d0887921f44683a81aea99820eb7d589a2
lidianevm <lidianevalentim_@hotmail.com>
2024-05-25 12:50:10 -0300

    Cenario 007

--- pages/HomePage.py
+++ pages/HomePage.py
@@ -4,8 +4,8 @@ from pages.PageObject import PageObject
 
 
 class HomePage(PageObject):
-
     url = 'https://demowebshop.tricentis.com/'
+    jewelryButton = (By.XPATH, "//*[@class='top-menu']//*[contains(@href, 'jewelry')]")
 
     def __init__(self, browser):
         super(HomePage, self).__init__(browser=browser)
@@ -16,4 +16,6 @@ class HomePage(PageObject):
     def is_url_home_page(self):
         return self.is_url(self.url)
 
+    def click_on_jewelry(self):
+        self.driver.find_element(*self.jewelryButton).click()
 
